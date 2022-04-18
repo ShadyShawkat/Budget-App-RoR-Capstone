09fb9c44b3c1159f0f2e98b01653490fc0c96dd7
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-19 00:57:25 +0200

    Edit 'Name' in User to be 'name'

--- db/migrate/20220418221922_devise_create_users.rb
+++ db/migrate/20220418221922_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :Name
+      t.string :name
 
       t.timestamps null: false
     end
