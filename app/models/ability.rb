165af734beb676238cc8288b20632ff8ce364bd0
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-19 21:25:51 +0200

    Add cancancan ability

---
+++ app/models/ability.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, all, public: true
+
+    return unless user.present?  # additional permissions for logged in users (they can read their own posts)
+    can :manage, all, user: user
+  end
+end
