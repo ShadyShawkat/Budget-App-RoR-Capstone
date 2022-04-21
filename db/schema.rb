f24fe314429479ae97b68e304946481fc1865151
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-21 19:06:24 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_19_154711) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_205401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_payments", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "payment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_payments_on_category_id"
+    t.index ["payment_id"], name: "index_categories_payments_on_payment_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -43,5 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_19_154711) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
+  add_foreign_key "categories_payments", "categories"
+  add_foreign_key "categories_payments", "payments"
   add_foreign_key "payments", "users"
 end
