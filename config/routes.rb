19298bfb6c6f158bc3f962dd980043f4d8982108
ShadyShawkat <shadyshawkat91@gmail.com>
2022-04-21 23:42:48 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
-  resources :payments
-  resources :categories
+  resources :payments, only: [:new, :index, :create]
+  resources :categories, except: [:update, :edit]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
